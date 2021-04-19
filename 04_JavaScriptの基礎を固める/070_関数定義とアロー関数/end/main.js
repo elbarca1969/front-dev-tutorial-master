9fc0b84f20ed71efbbde2944322447b05118c660
elbarca1969 <72918547+elbarca1969@users.noreply.github.com>
2021-04-19 17:32:26 +0900

    関数定義とアロー関数

--- 04_JavaScriptの基礎を固める/070_関数定義とアロー関数/end/main.js
+++ 04_JavaScriptの基礎を固める/070_関数定義とアロー関数/end/main.js
@@ -3,3 +3,4 @@ const hello = (name, age) => `${name} ${age}`;
 const arry = [1,2,3,4,5,6];
 
 arry.forEach(value => console.log(value));
+ 
---
+++ 04_JavaScriptの基礎を固める/070_関数定義とアロー関数/start/main.js
@@ -0,0 +1,27 @@
+// function hello(name = 'Tom') {
+//   console.log('hello ' + name);
+// }
+
+// const hello = function(name = 'Tom') {
+//   console.log('hello ' + name);
+// }
+
+// const hello = (name = 'Tom') => console.log('hello ' + name);
+// const hello = name => console.log('hello ' + name);
+
+// const hello = (name, age) => console.log('hello ' + name + age);
+
+// const hello = (name = 'Tom') => {
+//   console.log('hello ' + name + age);
+//   console.log('hello ' + name + age);
+// }
+
+// hello('Code Mafia');
+// hello('Code Mafia 2');
+// hello();
+
+const hello = (name, age) => 40;
+
+const arry = [1,2,3,4,5,6];
+
+arry.forEach(value => console.log(value));
