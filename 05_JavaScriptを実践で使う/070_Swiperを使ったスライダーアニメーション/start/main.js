707f4d31a92365dac1cf9288eb6af27a1db9b9bc
elbarca1969 <72918547+elbarca1969@users.noreply.github.com>
2021-04-22 16:29:31 +0900

    Swiperを使ったスライダーアニメーション＿実践Part3

--- 05_JavaScriptを実践で使う/070_Swiperを使ったスライダーアニメーション/start/main.js
+++ 05_JavaScriptを実践で使う/070_Swiperを使ったスライダーアニメーション/start/main.js
@@ -1,6 +1,5 @@
-const swiper = new Swiper('.swiper-container', {
-  // Optional parameters
-  // direction: 'vertical',
-  loop: true,
-  effect: 'coverflow',
+document.addEventListener('DOMContentLoaded', function () {
+  const hero = new HeroSlider('.swiper-container');
+  hero.start();
 });
+
